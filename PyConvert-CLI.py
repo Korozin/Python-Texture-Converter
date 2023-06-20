b01e4b9da0778b404fe0f6213b80aded2204e6db
Korozin <90534409+Korozin@users.noreply.github.com>
2023-06-20 01:32:08 -0400

    Update PyConvert-CLI.py

--- PyConvert-CLI.py
+++ PyConvert-CLI.py
@@ -393,7 +393,7 @@ def check_fallback():
                 zip_ref.extractall("fallback")
             os.remove('fallback.zip')
         else:
-            url = "https://korozin.github.io/files/fallback.zip"
+            url = "https://korozin.github.io/assets/downloads/textures/fallback.zip"
             response = requests.get(url)
             open("fallback.zip", "wb").write(response.content)
             with ZipFile("fallback.zip", "r") as zip_ref:
